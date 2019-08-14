c27fd187914570d9933f91a6a98c1206782c17fb
komagic <910367438@qq.com>
2019-08-14 20:18:16 +0800

    push build

---
+++ build/precache-manifest.1f6be731309c9b9e552b0a3c49717526.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2a2549d97554235e57d1d034029f29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73c229d6f070a893ef45",
+    "url": "/static/css/2.65053fe6.chunk.css"
+  },
+  {
+    "revision": "cb10084b2718a8f5e7b7",
+    "url": "/static/css/main.fd927a3b.chunk.css"
+  },
+  {
+    "revision": "73c229d6f070a893ef45",
+    "url": "/static/js/2.7389f368.chunk.js"
+  },
+  {
+    "revision": "cb10084b2718a8f5e7b7",
+    "url": "/static/js/main.4b7d99be.chunk.js"
+  },
+  {
+    "revision": "771facdc260f9aa42f43",
+    "url": "/static/js/runtime~main.67b05caf.js"
+  },
+  {
+    "revision": "7a4a2d30d5e1dafadd22f69fe8a58e82",
+    "url": "/static/media/beihufa.7a4a2d30.png"
+  },
+  {
+    "revision": "593dcc3d018dbb738e5f4d8b8bdfc674",
+    "url": "/static/media/imageCut.593dcc3d.png"
+  },
+  {
+    "revision": "9c5936632603e4402ddd6382179cfc33",
+    "url": "/static/media/qinghaiban.9c593663.jpg"
+  },
+  {
+    "revision": "68a4db80f66e6fa1a1da2bae53fd6ee4",
+    "url": "/static/media/support.68a4db80.jpg"
+  },
+  {
+    "revision": "7bfc4c743122a2c1bfb928d635de1316",
+    "url": "/static/media/weatherchart.7bfc4c74.png"
+  },
+  {
+    "revision": "d14a40a7ee75f4e66c28e54007a13a47",
+    "url": "/static/media/xiaobaowei.d14a40a7.jpeg"
+  },
+  {
+    "revision": "a862bc47d34d74179c3e03d4d633ae30",
+    "url": "/static/media/xiaofeizhe.a862bc47.jpeg"
+  }
+]);
